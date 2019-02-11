1142d344c0c0c70306171d45a682dc9cda1e04fd
Edwy Lugo <edwy.lugo@exatron.com.br>
2019-02-11 19:19:55 -0200

    Initial Commit

---
+++ Gerador numeros/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gerador numeros
+//
+//  Created by Edwy Lugo on 11/02/19.
+//  Copyright © 2019 SDvirtua Marketing Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gerador numerosTests/Gerador_numerosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gerador_numerosTests.swift
+//  Gerador numerosTests
+//
+//  Created by Edwy Lugo on 11/02/19.
+//  Copyright © 2019 SDvirtua Marketing Digital. All rights reserved.
+//
+
+import XCTest
+@testable import Gerador_numeros
+
+class Gerador_numerosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
